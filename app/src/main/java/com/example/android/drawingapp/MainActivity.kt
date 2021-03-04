76de70edda2db9a60455503fbc9d5f4aeca8c704
Prince Pandey <princekupandey786@gmail.com>
2021-03-04 22:13:25 +0530

    Added Share feature

--- app/src/main/java/com/example/android/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/android/drawingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -211,7 +212,21 @@ class MainActivity : AppCompatActivity() {
                         "Something went wrong wile saving the file",
                         Toast.LENGTH_SHORT).show()
             }
+            MediaScannerConnection.scanFile(this@MainActivity, arrayOf(result),null){
+                path , uri -> val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.putExtra(Intent.EXTRA_STREAM,uri)
+                shareIntent.type = "image/png"
+
+                startActivity(
+                        Intent.createChooser(
+                                shareIntent,"Share"
+                        )
+                )
+            }
+
         }
+
 
     }
 
